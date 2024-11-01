038de2e24b76dcd3bdf9afc83e182257af1d47d1
Joel Chemmanur <joelchem@Joels-MacBook-Air.local>
2024-11-01 17:02:34 -0400

    fixed dockerfile and added distance-service stuff

--- services/distance_service/app.py
+++ services/distance_service/app.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Title": "Hello World", "Description": "This is the distance service!!"}
+
--- services/distance_service/dev.bat
+++ services/distance_service/dev.bat
@@ -1,1 +1,1 @@
-docker run --rm -it -v .\:/app -w /app python:3.9-alpine sh
+docker run --rm -it -p 8000:8000 -v .\:/app -w /app python:3.9-alpine sh
