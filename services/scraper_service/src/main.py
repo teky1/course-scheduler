8ac1fc018dda638dfa279f1f13fafc6bdcdee09f
Teky1 <officialteky@gmail.com>
2024-11-22 00:57:13 -0500

    [in progress] course parser

--- services/scraper_service/src/main.py
+++ services/scraper_service/src/main.py
@@ -19,7 +19,7 @@ async def main():
     utils.log(start, "Getting semesters...")
     semesters = await schedule.get_semesters()
     utils.log(start, "Done")
-
+    
     for semester in semesters:
         utils.log(start, f"Processing semester {semester}...")
         depts = await schedule.get_dept_list(semester)
--- services/scraper_service/src/schedule.py
+++ services/scraper_service/src/schedule.py
@@ -2,6 +2,7 @@ import datetime
 import re
 import asyncio
 import itertools
+import json
 
 import httpx
 from bs4 import BeautifulSoup, NavigableString
@@ -11,7 +12,8 @@ import utils
 async def get_semesters():
     year = datetime.datetime.now().year
     years = (year-1, year, year+1)
-    terms = ("01", "05", "08", "12")
+    # terms = ("01", "05", "08", "12")
+    terms = ("01", "08") # only support fall and spring for now
     semesters = [f"{y}{t}" for y in years for t in terms]
 
     async with httpx.AsyncClient() as client:
@@ -31,7 +33,7 @@ async def get_dept_list(semester):
     url = f"https://app.testudo.umd.edu/soc/{semester}"
     res = await utils.single_get(url)
 
-    soup = BeautifulSoup(res.text, "html.parser")
+    soup = BeautifulSoup(res.text, "lxml")
 
     prefix_spans = soup.find_all(class_="prefix-abbrev")
     right_col = soup.find(id="right-course-prefix-column")
@@ -49,10 +51,61 @@ async def get_dept(semester, dept, client):
     return {tag.get("id"):tag for tag in courses}
 
 def parse_raw_courses(courses):
+    all_data = {}
     for course in courses:
-        validate_raw_course(courses[course], "root")
-        exit()
+        all_data[course] = parse_course(courses[course])
+    
+    with open("out.json", "w") as f:
+        json.dump(all_data, f, indent=2)
+    
+    exit()
 
-def validate_raw_course(tag, mark, depth=0):
-    print(f"{'  '*depth}{str(tag).split('>')[0]}>")
+def parse_course(root):
+
+    course_data = {}
+
+    course_data["id"] = root.find_all(class_="course-id")[0].text
+
+    course_data["name"] = root.find_all(class_="course-title")[0].text
+    course_data["reqs"] = {}
+    course_data["desc_notes"] = ""
+    course_data["desc"] = ""
+
+    print(course_data)
+
+    for elem in root.find_all(class_="course-text"):
+        course_data["desc_notes"] = "\n".join([line.strip() for line in list(elem.strings)])+"\n"
+    
+    for e in root.find_all(class_="individual-instruction-message"):
+        course_data["desc_notes"] = "\n".join([line.strip() for line in list(e.strings)])+"\n"
+
+    for text_elements in root.find_all(class_="approved-course-text"):
+
+        notes = [tag for tag in text_elements.find_all("div") if tag.find_all("strong", recursive=False)]
+        
+        if len(notes) == 0:
+
+            course_data["desc"] += text_elements.text+"\n"
+            continue
+        
+        for note in notes:
+            
+            if note.text.strip() == "":
+                continue
+            parts = [text.strip().replace(":","") for text in note.strings]
+
+            course_data["reqs"][parts[0]] = parts[1]
+
+    print(course_data)
+
+    return course_data
+
+def parse_section(root):
+    section_data = {}
+
+
+
+
+
+
     
