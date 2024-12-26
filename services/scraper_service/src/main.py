5c2f1f9bd4405e97bcb1a985762c18117e204896
Teky1 <officialteky@gmail.com>
2024-12-26 02:49:04 -0500

    more section parsing data--still WIP

--- services/scraper_service/src/main.py
+++ services/scraper_service/src/main.py
@@ -26,7 +26,7 @@ async def main():
         courses = {}
 
         async with httpx.AsyncClient() as client:
-            concurrent = 25
+            concurrent = 50
             for i in range(0,len(depts), concurrent):
                 results = await asyncio.gather(
                     *(schedule.get_dept(semester, dept, client) for dept in depts[i:i+concurrent]))
@@ -34,10 +34,11 @@ async def main():
                 for res in results:
                     for course in res:
                         courses[course] = res[course]
+                
+                await asyncio.sleep(0.5)
 
                 # utils.log(start, f"({min(i+concurrent, len(depts))}/{len(depts)}) depts retrieved")
 
-                await asyncio.sleep(0.5)
         
 
         utils.log(start, f"{len(courses)} courses loaded total")
@@ -46,7 +47,7 @@ async def main():
         course_data = schedule.parse_raw_courses(courses)
 
         utils.log(start, "Parsing sections..")
-        section_data = await schedule.get_sections(list(course_data.keys()))
+        section_data = await schedule.get_sections(semester, list(course_data.keys()))
 
         utils.log(start, "Updating DB...")
         # db.update_all_courses(semester, course_data)
--- services/scraper_service/src/schedule.py
+++ services/scraper_service/src/schedule.py
@@ -50,25 +50,66 @@ async def get_dept(semester, dept, client):
     return {tag.get("id"):tag for tag in courses}
 
 # courses is a list of course ids
-async def get_sections(courses):
+async def get_sections(semester, courses):
     out = {}
     while len(courses) > 0:
-        url = "https://app.testudo.umd.edu/soc/202501/sections?courseIds="+courses.pop()
-        while len(url) < 6000 and len(courses) > 0:
+        url = f"https://app.testudo.umd.edu/soc/{semester}/sections?courseIds="+courses.pop()
+        while len(url) < 2000 and len(courses) > 0:
             url += f",{courses.pop()}"
+
         res = await utils.single_get(url)
 
         soup = BeautifulSoup(res.text, "html.parser")
         courses_found = soup.find_all(class_="course-sections")
 
         for course in courses_found:
-            sections = parse_sections(course)
-            print(sections)
+            course_id = course.get("id")
+            out[course_id] = {"footnote": ""}
+            sections = course.find_all(class_="section")
+
+            for msg in course.find_all(class_="footnote-message"):
+                out[course_id]["footnote"] += msg.text.strip()+"\n"
+
+
+            for section in sections:
+                section_data = parse_section(section)
+                out[course_id][section_data["section_id"]] = section_data
+
         
 
 
-def parse_sections(root):
-    pass # write section parsing here
+    with open("section-out.json", "w") as f:
+        json.dump(out, f, indent=2)
+        print(f"{sum([len(x.keys()) for x in out.values()])} sections data dumped to section-out.json")
+    
+    return out
+        
+
+
+def parse_section(root):
+    out = {}
+    out["section_id"] = root.find_all(class_="section-id")[0].text.strip()
+
+    out["total_seats"] = int(root.find_all(class_="total-seats-count")[0].text.strip())
+    out["open_seats"] = int(root.find_all(class_="open-seats-count")[0].text.strip())
+
+    waitlist_elements = root.find_all(class_="waitlist-count")
+
+    out["waitlist"] = 0 if len(waitlist_elements) < 1 else int(waitlist_elements[0].text.strip())
+
+    if len(waitlist_elements) > 1:
+        out["holdfile"] = int(waitlist_elements[1].text.strip())
+
+    out["instructors"] = [instruc.text.strip() for instruc in root.find_all(class_="section-instructor")]
+
+    out["footnote_marked"] = len(root.find_all(class_="footnote-marker")) > 0
+
+    
+    
+    # online or in person or wte?
+    # days/time/location
+    
+    return out
 
 
 def parse_raw_courses(courses):
@@ -140,10 +181,6 @@ def parse_course(root):
 
     return course_data
 
-
-
-def parse_section(root):
-    section_data = {}
 
 
 
