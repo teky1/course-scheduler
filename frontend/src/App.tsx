111209e805f53f6afb323d0b895067af37c5857e
Teky1 <officialteky@gmail.com>
2025-02-09 12:22:26 -0500

    removed ALL unused code i think

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import '@mantine/core/styles.css';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 import {AppShell, MantineProvider } from '@mantine/core';
 import Navbar from './components/navbar/Navbar';
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <MantineProvider forceColorScheme='dark'>
