b9c028a989f6446be5ddbecd8304a5c80e2525e1
Teky1 <officialteky@gmail.com>
2025-03-11 00:13:22 -0400

    more of the course stuff

--- services/api/routers/courses.py
+++ services/api/routers/courses.py
@@ -1,48 +1,77 @@
 from fastapi import APIRouter
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import redis
 
 import re
 import logging
+import orjson
+
+RESULT_LIMIT = 20
 
 router = APIRouter()
 
 with open("db_uri.txt") as f:
     uri = f.read()
 
-client = MongoClient(uri, server_api=ServerApi('1'))
-db = client["CourseNotifierCluster"]
 
-client.admin.command('ping')
+
+mongo = MongoClient(uri, server_api=ServerApi('1'))
+mongo.admin.command('ping')
 logging.info("API Service Connected to MongoDB")
 
-RESULT_LIMIT = 20
+cache = redis.Redis(host="redis", port=6379)
+if cache.ping():
+    logging.info("API Service Connected to Redis")
+else:
+    logging.error("API could NOT connect to Redis")
+
+
 
 @router.get("/search/{semester}")
 async def search(semester: int, query: str, offset: int = 0): 
-    # need to add regex cacheing
-    # need to verify semester input
-    collection = db[f"courses-{semester}"]
+    
+    cache_key = f"{semester}:{offset}:{query}"
+    cached = cache.get(cache_key)
+    if cached:
+        return orjson.loads(cached)
 
+    if not await verify_semester(semester):
+        return {"error": "invalid semester"}
+
+    collection = mongo["CourseNotifierCluster"][f"courses-{semester}"]
+
+    res=None
     if re.search(r'^([A-Z]{4}\d{3}[A-Z]?|[A-Z]{1,4}|[A-Z]{4}\d{1,3})$', query.strip().upper()):
-
         res = list(
-            collection.find({"_id": {"$regex": f"^{re.escape(query)}"}})
+            collection.find({
+                "$or": [
+                    {"_id": {"$regex": f"^{re.escape(query.strip().upper())}"}},
+                    {"name": {"$regex": re.escape(query.strip()), "$options": "i"}}
+                ]
+            })
             .sort("_id", 1)
             .skip(offset)
             .limit(RESULT_LIMIT)
         )
+    else:
+        res = list(
+            collection.find({"name": {"$regex": re.escape(query.strip()), "$options": "i"}})
+            .skip(offset)
+            .limit(RESULT_LIMIT)
+        )
 
-        await hydrate_courses(res, semester)
+    await hydrate_courses(res, semester)
 
-        if len(res) > 0:
-            return {"size": len(res), "courses": res}
-    
-    # remain regex maybe {name: {$regex: "rith", $options: "i"}}
+    out = {"size": len(res), "courses": res}
+
+    cache.setex(cache_key, 5*60, orjson.dumps(out))
+    return out
+
     
 async def hydrate_courses(courses, semester):
     
-    collection = db[f"sections-{semester}"]
+    collection = mongo["CourseNotifierCluster"][f"sections-{semester}"]
 
     ids = [course["_id"] for course in courses]
     res = collection.find({"_id": {"$in": ids}})
@@ -51,3 +80,16 @@ async def hydrate_courses(courses, semester):
     
     for course in courses:
         course["sections"] = [] if course["_id"] not in sections_map else sections_map[course["_id"]]
+
+async def verify_semester(semester):
+
+    if cache.get(semester):
+        return True
+ 
+    collection = mongo["Metadata"]["all-semesters"]
+    semesters = list(collection.find())
+
+    for sem in semesters:
+        cache.set(sem["_id"], 1)
+
+    return bool(cache.get(semester))
