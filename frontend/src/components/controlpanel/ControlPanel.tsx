4fb4cb2f14cb3ecc9f5e245cc6ec227f0dbdf26e
Teky1 <officialteky@gmail.com>
2025-04-02 14:00:30 -0400

    exit capture for control panel

--- frontend/src/components/controlpanel/ControlPanel.tsx
+++ frontend/src/components/controlpanel/ControlPanel.tsx
@@ -25,18 +25,25 @@ function ControlPanel() {
     })
 
     return (
-        <div className={`${styles.root} ${(appContext?.controlPanelToggled) ? styles.rootOpen : ""}`}>
-            <Modal
-                opened={urlModal}
-                onClose={() => setUrlModal(false)}
-                title="Share Link"
-                removeScrollProps={{enabled: false}}
-                classNames={{ content: styles.urlModalRoot, header: styles.urlModalRoot, title: styles.urlModalTitle }}
-            >
-                <LoadingOverlay visible={urlLoading} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} loaderProps={{ color: "#fe4d4d" }} />
-                <span ref={urlRef} className={styles.url}>{url}</span>
-                {/* <button className={styles.urlCopyBtn} onClick={() => {urlRef.current?.select()}}>Copy</button> */}
-                {/* <Popover position="bottom" withArrow shadow="md">
+        <>
+            {appContext?.controlPanelToggled ? (
+                <div
+                    className={styles.exitCapture}
+                    onClick={() => appContext?.setControlPanelToggle(false)}
+                ></div>
+            ) : null}
+            <div className={`${styles.root} ${(appContext?.controlPanelToggled) ? styles.rootOpen : ""}`}>
+                <Modal
+                    opened={urlModal}
+                    onClose={() => setUrlModal(false)}
+                    title="Share Link"
+                    removeScrollProps={{ enabled: false }}
+                    classNames={{ content: styles.urlModalRoot, header: styles.urlModalRoot, title: styles.urlModalTitle }}
+                >
+                    <LoadingOverlay visible={urlLoading} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} loaderProps={{ color: "#fe4d4d" }} />
+                    <span ref={urlRef} className={styles.url}>{url}</span>
+                    {/* <button className={styles.urlCopyBtn} onClick={() => {urlRef.current?.select()}}>Copy</button> */}
+                    {/* <Popover position="bottom" withArrow shadow="md">
                     <Popover.Target>
                         
                     </Popover.Target>
@@ -44,90 +51,92 @@ function ControlPanel() {
                         Link Copied
                     </Popover.Dropdown>
                 </Popover> */}
-            </Modal>
-            <div className={`${styles.saveButtonsContainer}`}>
-                <Button
-                    classNames={{ label: styles.btnLabel, root: styles.btn }}
-                    leftSection={<i className="fa-solid fa-link"></i>}
-                    onClick={() => {
-                        setUrlModal(true);
-                        setUrlLoading(true);
+                </Modal>
+                <div className={`${styles.saveButtonsContainer}`}>
+                    <Button
+                        classNames={{ label: styles.btnLabel, root: styles.btn }}
+                        leftSection={<i className="fa-solid fa-link"></i>}
+                        onClick={() => {
+                            setUrlModal(true);
+                            setUrlLoading(true);
 
-                        let sections = appContext?.selectedSections.map(([course, section]) => {
-                            return course._id+"-"+section.section_id
-                        })
+                            let sections = appContext?.selectedSections.map(([course, section]) => {
+                                return course._id + "-" + section.section_id
+                            })
 
-                        axios.post("https://api.scheduleterp.com/schedule/upload", {
-                            semester: "202508",
-                            sections,
-                            colors: sections?.map((sec) => appContext?.colorMap[sec])
-                        }).then((resp) => {
-                            if(resp.data.success) {
-                                setUrl("http://"+window.location.host+"/share/"+resp.data.id);
-                                
-                            } else {
-                                setUrl("Error: "+resp.data.error);
-                            }
-                            setUrlLoading(false);
-                        });
-                    }}
-                >Share Link</Button>
-                <Button
-                    classNames={{ label: styles.btnLabel, root: styles.btn }}
-                    leftSection={<i className="fa-regular fa-calendar"></i>}
-                    onClick={() => alert("Export to calendar coming soon....")}
-                >Export Calendar</Button>
+                            axios.post("https://api.scheduleterp.com/schedule/upload", {
+                                semester: "202508",
+                                sections,
+                                colors: sections?.map((sec) => appContext?.colorMap[sec])
+                            }).then((resp) => {
+                                if (resp.data.success) {
+                                    setUrl("http://" + window.location.host + "/share/" + resp.data.id);
+
+                                } else {
+                                    setUrl("Error: " + resp.data.error);
+                                }
+                                setUrlLoading(false);
+                            });
+                        }}
+                    >Share Link</Button>
+                    <Button
+                        classNames={{ label: styles.btnLabel, root: styles.btn }}
+                        leftSection={<i className="fa-regular fa-calendar"></i>}
+                        onClick={() => alert("Export to calendar coming soon....")}
+                    >Export Calendar</Button>
+
+                </div>
+                <div className={styles.scheduleSelectContainer}>
+                    <i
+                        className={`${styles.dropdownIcon} fa-solid fa-caret-${nameDropdownToggled ? "down" : "right"}`}
+                        onClick={() => setNameDropdownToggle(last => !last)}
+                    ></i>
+                    <input
+                        className={styles.nameEditBox}
+                        value={appContext?.currentScheduleName}
+                        onChange={e => appContext?.setCurrentScheduleName(e.target.value.substring(0, 32))}
+                        onBlur={() => appContext?.setCurrentScheduleName(last => last.trim() ? last : "Unnamed Schedule")}
+                    ></input>
+                    <i
+                        className={`${styles.addBtn} fa-solid fa-plus`}
+                        onClick={() => {
+                            let sched = createSchedule();
+                            let id = sched.id;
+
+                            saveSchedule(sched);
+                            appContext?.setCurrentScheduleID(id);
+                            setNameDropdownToggle(true);
+                        }}></i>
+                </div>
+                <Collapse className={styles.otherSchedulesContainer} in={nameDropdownToggled}>
+                    {
+                        appContext?.schedulesList.map(schedule => ((schedule.id == appContext.currentScheduleID) ? null :
+                            <div className={styles.otherSchedules}
+                                key={schedule.id}>
+                                <span className={styles.otherScheduleTitle}
+                                    onClick={() => appContext.setCurrentScheduleID(schedule.id)}
+                                >{schedule.name}</span>
+                                <i className={`${styles.deleteScheduleBtn} fa-solid fa-trash`}
+                                    onClick={() => {
+                                        let confirm = window.confirm(`Are you sure you want to delete \"${schedule.name}\"?`);
+                                        if (!confirm) { return; };
+                                        deleteSchedule(schedule.id);
+                                        appContext.setSchedulesList(() => getScheduleList());
+                                    }}></i>
+                            </div>
+                        ))
+                    }
+                </Collapse>
+                <Divider my="sm" color="var(--mantine-color-gray-7)" />
+                <span className={styles.credits}>Credits: {minCredits}{(maxCredits > minCredits) ? "-" + maxCredits : ""}</span>
+                <div className={styles.sectionsContainer}>
+                    {appContext?.selectedSections.map(section => <SectionComponent section={section} />)}
+                </div>
+
 
             </div>
-            <div className={styles.scheduleSelectContainer}>
-                <i
-                    className={`${styles.dropdownIcon} fa-solid fa-caret-${nameDropdownToggled ? "down" : "right"}`}
-                    onClick={() => setNameDropdownToggle(last => !last)}
-                ></i>
-                <input
-                    className={styles.nameEditBox}
-                    value={appContext?.currentScheduleName}
-                    onChange={e => appContext?.setCurrentScheduleName(e.target.value.substring(0, 32))}
-                    onBlur={() => appContext?.setCurrentScheduleName(last => last.trim() ? last : "Unnamed Schedule")}
-                ></input>
-                <i
-                    className={`${styles.addBtn} fa-solid fa-plus`}
-                    onClick={() => {
-                        let sched = createSchedule();
-                        let id = sched.id;
+        </>
 
-                        saveSchedule(sched);
-                        appContext?.setCurrentScheduleID(id);
-                        setNameDropdownToggle(true);
-                    }}></i>
-            </div>
-            <Collapse className={styles.otherSchedulesContainer} in={nameDropdownToggled}>
-                {
-                    appContext?.schedulesList.map(schedule => ((schedule.id == appContext.currentScheduleID) ? null :
-                        <div className={styles.otherSchedules}
-                            key={schedule.id}>
-                            <span className={styles.otherScheduleTitle}
-                                onClick={() => appContext.setCurrentScheduleID(schedule.id)}
-                            >{schedule.name}</span>
-                            <i className={`${styles.deleteScheduleBtn} fa-solid fa-trash`}
-                                onClick={() => {
-                                    let confirm = window.confirm(`Are you sure you want to delete \"${schedule.name}\"?`);
-                                    if (!confirm) { return; };
-                                    deleteSchedule(schedule.id);
-                                    appContext.setSchedulesList(() => getScheduleList());
-                                }}></i>
-                        </div>
-                    ))
-                }
-            </Collapse>
-            <Divider my="sm" color="var(--mantine-color-gray-7)" />
-            <span className={styles.credits}>Credits: {minCredits}{(maxCredits > minCredits) ? "-"+maxCredits : ""}</span>
-            <div className={styles.sectionsContainer}>
-                {appContext?.selectedSections.map(section => <SectionComponent section={section} />)}
-            </div>
-
-
-        </div>
     )
 }
 
