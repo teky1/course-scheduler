f7e28115e7e663f93fa87628ccd1c5e0ca6e7f87
Teky1 <officialteky@gmail.com>
2025-04-02 12:05:46 -0400

    always red fix

--- frontend/src/components/schedule/MeetingBlock.tsx
+++ frontend/src/components/schedule/MeetingBlock.tsx
@@ -3,7 +3,7 @@ import styles from "./schedule.module.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { Day } from "../../types/api";
 import { timeToPercent } from "./utils/schedulePlacement";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../app/App";
 import { minifyTimeCode } from "../../utils/sectionUtils";
 
@@ -11,6 +11,17 @@ const BORDER_RADIUS = 0.2; //em
 const MARGIN = 0.25 + 2 * BORDER_RADIUS; // em
 
 const COLORS = [0, 30, 60, 120, 180, 210, 230, 260, 300];
+const days: Day[] = ["M", "Tu", "W", "Th", "F"];
+
+function addSpace(courseID: string): string {
+  const match = courseID.match(/\d/);
+  if (!match) return courseID;
+
+  const index = match.index!;
+  return courseID.slice(0, index) + "​" + courseID.slice(index);
+}
+
+let getDayLeft: (day: Day) => number = (day) => days.indexOf(day) * 20;
 
 let MeetingBlock: MeetingBlockComponent = ({
   block,
@@ -23,19 +34,6 @@ let MeetingBlock: MeetingBlockComponent = ({
 
   let appContext = useContext(AppContext);
 
-  function addSpace(courseID: string): string {
-    const match = courseID.match(/\d/);
-    if (!match) return courseID;
-
-    const index = match.index!;
-    return courseID.slice(0, index) + "​" + courseID.slice(index);
-  }
-
-
-  let days: Day[] = ["M", "Tu", "W", "Th", "F"];
-
-  let getDayLeft: (day: Day) => number = (day) => days.indexOf(day) * 20;
-
   if (block.day == "Other") {
     return <></>;
   }
@@ -45,12 +43,23 @@ let MeetingBlock: MeetingBlockComponent = ({
   //     section[0]._id == block.course._id &&
   //     section[1].section_id == block.section.section_id
   // );
+  
   let sectionCode = block.course._id+"-"+block.section.section_id;
-  if(!(sectionCode in ((appContext) ? appContext.colorMap : {}))) {
-    appContext?.setColorMap((last) => {
-      return ({ ...last, [sectionCode]: COLORS[Math.floor(Math.random() * COLORS.length)]}); 
-      // return ({ ...last, [sectionCode]: Math.floor(Math.random() * 360)}); 
-    });
+  
+  useEffect(() => {
+    if(appContext && !(sectionCode in appContext.colorMap)) {
+      let color = COLORS[Math.floor(Math.random() * COLORS.length)];
+      // console.log(sectionCode, color)
+      // console.log(appContext.colorMap)
+      appContext?.setColorMap((last) => {
+        return ({ ...last, [sectionCode]: color}); 
+        // return ({ ...last, [sectionCode]: Math.floor(Math.random() * 360)}); 
+      });
+    }
+  }, [sectionCode, appContext]);
+
+  if(!(appContext?.colorMap[sectionCode])) {
+    return null;
   }
   return (
     <div
