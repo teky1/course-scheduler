c562b1ba85c0abe5ba0fa5377ca30a1083c1f8f5
Teky1 <officialteky@gmail.com>
2025-04-02 02:43:25 -0400

    optimized traveltime service

--- services/api/routers/traveltime.py
+++ services/api/routers/traveltime.py
@@ -4,67 +4,30 @@ import httpx
 from bs4 import BeautifulSoup
 from fastapi import APIRouter
 import logging
+import orjson
+import db
 
 router = APIRouter()
+
+with open("locations.json") as f:
+    location_map = orjson.loads(f.read())
 
 @router.get("/traveltime/{loc1}/{loc2}")
 async def traveltime(loc1: str, loc2: str, mode: str = "foot"):
 
-    coord1, coord2 = await asyncio.gather(
-        get_coords(loc1),
-        get_coords(loc2)
-    )
+    cache_key = f"travel:{loc1}:{loc2}:{mode}"
+    cached = db.cache.get(cache_key)
+    if cached:
+        return orjson.loads(cached)
     
-    time_secs = await get_travel_time(coord1, coord2, mode)
+    time_secs = await get_travel_time(location_map[loc1], location_map[loc2], mode)
 
     time_mins = round(time_secs/60, 2)
-    return {"time_mins": time_mins, "time_secs": time_secs}
+    out = {"time_mins": time_mins, "time_secs": time_secs}
 
+    db.cache.setex(cache_key, 60*60*24*7, orjson.dumps(out))
 
-async def get_coords(loc: str):
-    code = await get_numeric_building_code(loc)
-    logging.info(f"Received Code for {loc}")
-    coords = await get_coords_from_building_code(code)
-    logging.info(f"Received Coords for {loc}")
-    return coords
-
-async def get_numeric_building_code(loc: str):
-    if loc.strip().isnumeric():
-        return int(loc.strip())
-    
-    bldg_code_endpoint = f"https://app.testudo.umd.edu/soc/buildings/{loc} 0"
-
-    async with httpx.AsyncClient(timeout=30) as client:
-        response = await client.get(bldg_code_endpoint)
-        response.raise_for_status()
-        html_content = response.text
-    
-    soup = BeautifulSoup(html_content, "html.parser")
-    bldg_code = soup.find_all("span", class_="code")[1].get_text()
-    return int(bldg_code)
-
-
-async def get_coords_from_building_code(code: str):
-    lat_long_data_url = "https://geodata.md.gov/imap/rest/services/Structure/MD_CampusFacilities/FeatureServer/2/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-    async with httpx.AsyncClient(timeout=30) as client:
-        response = await client.get(lat_long_data_url)
-        response.raise_for_status()
-        data = response.json()
-
-    for bldg in data["features"]:
-        if int(bldg["attributes"]["BUILDINGID"]) != int(code):
-            continue 
-        
-        vertices = bldg["geometry"]["rings"][0]
-        lat, long = 0, 0
-        for v in vertices:
-            long += v[0]
-            lat += v[1]
-        lat /= len(vertices)
-        long /= len(vertices)
-        return (lat, long)
-    
-    return None
+    return out
 
 
 async def get_travel_time(coord1, coord2, mode):
